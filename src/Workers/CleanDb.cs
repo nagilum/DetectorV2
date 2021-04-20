700c3d108ac27603365c463c463517f725056180
Stian Hanger <pdnagilum@gmail.com>
2021-04-20 13:41:03 +0200

    Add cleaning of old scan results

--- src/Workers/CleanDb.cs
+++ src/Workers/CleanDb.cs
@@ -43,6 +43,7 @@ namespace DetectorWorker.Workers
                 var removedIssues = 0;
                 var removedAlerts = 0;
                 var removedLogs = 0;
+                var removedScanResults = 0;
 
                 // Remove deleted resources older than 6 months.
                 try
@@ -144,8 +145,25 @@ namespace DetectorWorker.Workers
                     await Log.LogCritical(ex.Message);
                 }
 
+                // Remove scan results older than 6 months.
+                try
+                {
+                    var scanResults = await db.ScanResults
+                        .Where(n => n.Created < dt)
+                        .ToListAsync(cancellationToken);
+
+                    removedScanResults += scanResults.Count;
+                    db.ScanResults.RemoveRange(scanResults);
+                    await db.SaveChangesAsync(cancellationToken);
+                }
+                catch (Exception ex)
+                {
+                    this.Logger.LogCritical(ex, ex.Message);
+                    await Log.LogCritical(ex.Message);
+                }
+
                 // Log.
-                this.Logger.LogInformation($"Removed {removedResources} deleted resources, {removedIssues} resolved issues, {removedAlerts} alerts, and {removedLogs} log entries from the db.");
+                this.Logger.LogInformation($"Removed {removedResources} deleted resources, {removedIssues} resolved issues, {removedAlerts} alerts, {removedScanResults} scan results, and {removedLogs} log entries from the db.");
 
                 // Wait a day.
                 await Task.Delay(new TimeSpan(24, 0, 0), cancellationToken);
